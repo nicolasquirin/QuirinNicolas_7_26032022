3349ad8299e84c7fbc7a4572ba16502a478b2b5f
nicolasquirin <nicolasquirin22@gmail.com>
2022-05-21 20:08:53 +0200

    Ternaire-on-Thread_OK

--- front/client/src/pages/Home.js
+++ front/client/src/pages/Home.js
@@ -6,18 +6,18 @@ import Log from "../components/Log";
 import Thread from "../components/Thread";
 import { NavLink } from "react-router-dom";
 
-const Home = (show) => {
+const Home = () => {
   const uid = useContext(UidContext);
 
-  //Ternaire afin de voir le fil d'actualité que si la condition signin est true
-  return !show ? null : (
+  //Ternaire sur <Tread /> afin de voir le fil d'actualité que si la condition signin est true
+  return (
     <div className="home">
       <LeftNav />
       <div className="main">
         <div className="home-header">
           {uid ? <NewPostForm /> : <Log signin={true} signup={false} />}
         </div>
-        {uid ? <Thread /> : <show signin={true} signup={false} />}
+        <div className="thread">{uid ? <Thread /> : !uid}</div>
       </div>
       <div className="right-side">
         <div className="icons">
